52a96ed240bd8f5d07ddea1adb60281012ea977c
Di Mou <31651530+tamamitsune@users.noreply.github.com>
2021-05-11 10:00:10 +0800

    Add files via upload

---
+++ scripts/数据处理与bin方法.R
@@ -0,0 +1,55 @@
+library(ggplot2)
+library(dplyr)
+
+
+Ta <- rnorm(1000, mean = 26, sd = 5)
+TSV <- 0.2 * Ta - 5.2 + rnorm(1000, mean = 0, sd = 1)
+
+df <- read.csv("./data/TSV-Ta数据.csv")
+df.bin <- df %>% 
+  mutate(Tabin = cut_width(Ta, width = 1, center = 25)) %>% 
+  group_by(Tabin) %>% 
+  summarise(TSVmean = mean(TSV),
+            observations = length(TSV),
+            Tamean = mean(Ta))
+
+ls.raw <- summary(lm(data = df,formula = TSV~Ta))
+eqn.raw <- as.character(as.expression(
+  substitute(italic(TSV) == k * italic(T[a]) + b* "," ~~ italic(r)^2 ~ "=" ~ r2 ~~ italic(p) ~ "< 0.0001",
+             list(
+               k = format(ls.raw$coefficients[2,1], digits = 3),
+               b= format(ls.raw$coefficients[1,1], digits = 3),
+               r2 = format(ls.raw$r.squared, digits = 3)
+             )
+  )))
+
+ls.bin <- summary(lm(data = df.bin,formula = TSVmean~Tamean))
+eqn.bin <- as.character(as.expression(
+  substitute(italic(TSV) == k * italic(T[a]) + b* "," ~~ italic(r)^2 ~ "=" ~ r2 ~~ italic(p) ~ "< 0.0001",
+             list(
+               k = format(ls.bin$coefficients[2,1], digits = 3),
+               b= format(ls.bin$coefficients[1,1], digits = 3),
+               r2 = format(ls.bin$r.squared, digits = 3)
+             )
+  )))
+
+
+
+ggplot() +
+  geom_point(data = df, aes(x = Ta, y = TSV, col = "raw"),alpha = 0.2) +
+  geom_smooth(data = df, aes(x = Ta, y = TSV, col = "raw"), method = "lm") +
+  geom_point(data = df.bin, aes(x = Tamean, y = TSVmean, size = observations, col = "binned"), shape = 1) +
+  geom_smooth(data = df.bin, aes(x = Tamean, y = TSVmean, col = "binned"), 
+              method = "lm") +
+  theme_bw() +
+  annotate(geom = "text", x = 13.5, y = 1.5,
+           label= eqn.raw,
+           size = 2, 
+           parse=TRUE) +
+  annotate(geom = "text", x = 13.5, y = 3,
+           label= eqn.bin,
+           size = 2, 
+           parse=TRUE) 
+
+  
+
